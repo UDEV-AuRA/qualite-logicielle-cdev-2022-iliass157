d9d7833b6e9114120d92763280dcef1a5cbb9aa6
Iliass EL LAHIBI <iliassellahibi@gmail.com>
2022-06-22 10:22:50 +0200

    Ajout test unitaires classe Employe GetnbAnneesAnciente

--- src/test/java/com/ipiecoles/java/java350/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/EmployeTest.java
@@ -71,9 +71,7 @@ public class EmployeTest {
 
     @ParameterizedTest
     @CsvSource({
-            "'M12345',0,1,1.0,1700.0",
-            "'T12345',0,1,1,0,1000.0",
-            "'T12345',0,2,1,0,2500.0"
+            "'M12345',0,1,1.0,1700.0"
     })
     public void testGetPrimeAnnuelle(String matricule,
                                      Integer nbAnneesAnciennete,
